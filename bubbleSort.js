36bd82ace20f2677a9b16c5b259203fe19cdedd7
HayatH <61989790+Hasnain-commits@users.noreply.github.com>
2021-03-03 11:48:52 -0500

    Update bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,16 +1,20 @@
 
-function bubbleSort(data) {
-for (let i = data.length - 1; i >= 0; i--) {
-    for (let j = 0; j < i; j++) {
-        if (data[j] > data[j + 1]) {
-            let temp = data[j];
-            data[j] =  data[j + 1];
-            data[j + 1] = temp;
+function bubbleSort(array) {
+    let isSorted = false;
+    let last = array.length - 1;
+    while (!isSorted) {
+        isSorted = true
+        for (let i = 0; i < last; i++) {
+            if (array[i] > array[i + 1]) {
+                let temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+                isSorted = false;
+            }
         }
-        console.log(data);
+        last--;
     }
-
-}
+    return array;
 }
 
 bubbleSort([213,15,61,73,762,12,123,123,12,626,347,3587,32,2352352365,12,5,651,627,2376,8,2]);
